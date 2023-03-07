e8ec2505ff43f0b82d11cd5c708e4c433ffa9599
mohulshukla <mohul.shukla@gmail.com>
2023-03-07 01:09:23 +0100

    test

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import random
+# function testing whether or not a particular value is in the mandelbrot set
+
+def is_in_mandelbrot(c, max_iterations=100):
+    z = 0
+    for i in range(max_iterations):
+        z = z*z + c
+        if abs(z) > 2:
+            return False
+    return True
 
 
-
+print(is_in_mandelbrot(0.11 + 0j))
